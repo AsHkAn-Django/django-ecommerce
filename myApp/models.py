405a3f71b304c31adffa5cb8f2e176018b33bcac
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-11 11:55:01 +0300

    addfavorite, rating models, add some useful methods to book model

--- myApp/models.py
+++ myApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.conf import settings
+from decimal import Decimal
 
 
 
@@ -9,25 +10,63 @@ class Book(models.Model):
     title = models.CharField(max_length=264)
     author = models.CharField(max_length=264)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0)])
-    stock = models.IntegerField(validators=[MinValueValidator(0)]) 
-    
+    stock = models.IntegerField(validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to=f'uploads/{title}/')
+
     def get_absolute_url(self):
         return reverse('myApp:home')
 
-    def __str__(self):
-        return self.title
-    
     def stock_lower_than10(self):
         if self.stock < 10:
             return True
         return False
-        
-        
+
+    def get_average_rating(self):
+        ratings = self.book_ratings.all()
+        if not ratings:
+            return Decimal('0.0')
+        else:
+            total = sum(rating.rate for rating in ratings)
+        return Decimal(round(total / len(ratings), 1))
+
+
+    def get_rates_number(self):
+        return len(self.book_ratings.all())
+
+    def __str__(self):
+        return self.title
+
+
 class CartItem(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='cart_items')
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchased_items')
-    add_date = models.DateTimeField(auto_now_add=True)
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='purchased_items')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.book.title
- 
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_favorites')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_favorites')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} added {self.book.title}"
+
+
+class Rating(models.Model):
+    rate = models.DecimalField(max_digits=2, decimal_places=1, validators=(MaxValueValidator(5.0), MinValueValidator(1.0)))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_ratings')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_ratings')
+    review = models.CharField(max_length=250, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Each user have one rating per book
+        constraints = [
+            models.UniqueConstraint(fields=['book', 'user'], name='unnique_book_user_rating')
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.rate} to {self.book.title}"

8d0184316d21723eb99c2837a84408de4e469e76
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-16 20:08:34 +0300

    add ordering to api

--- myApp/api_views.py
+++ myApp/api_views.py
@@ -6,6 +6,9 @@ from rest_framework.decorators import action
 
 
 class BookViewset(viewsets.ModelViewSet):
+    """
+    A viewset with search and ordering.
+    """
     serializer_class = BookSerializer
 
     def get_queryset(self):
@@ -13,4 +16,19 @@ class BookViewset(viewsets.ModelViewSet):
         qs = Book.objects.all()
         if title:
             qs = qs.filter(title__icontains=title)
+
+        ordering = self.request.query_params.get('ordering')
+        if ordering:
+            qs = qs.order_by(ordering)
         return qs
+
+
+# TODO: We can also do the filtering part with DRF built-in
+# from rest_framework import filters
+
+# class BookViewset(viewsets.ModelViewSet):
+#     serializer_class = BookSerializer
+#     queryset = Book.objects.all()
+#     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+#     search_fields = ['title']
+#     ordering_fields = ['price', 'title', 'author']

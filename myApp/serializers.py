f9f20e2208f8ef023dcf83e70005b59a5e76be17
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-16 20:25:14 +0300

    add serializermethodfield for model methods

--- myApp/serializers.py
+++ myApp/serializers.py
@@ -3,7 +3,17 @@ from .models import Book, Rating, Favorite, CartItem
 
 
 class BookSerializer(serializers.ModelSerializer):
+    average_rate = serializers.SerializerMethodField()
+    rate_numbers = serializers.SerializerMethodField()
+
+
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'stock', 'price', 'image', 'description')
+        fields = ('id', 'title', 'author', 'stock', 'price', 'image', 'description', 'average_rate', 'rate_numbers')
         read_only_fields = ('author', 'stock',)
+
+    def get_average_rate(self, obj):
+        return obj.get_average_rating()
+
+    def get_rate_numbers(self, obj):
+        return obj.get_rates_number()
